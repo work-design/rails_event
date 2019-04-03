b0c39af9e0ab200f3b910f6b360dd239a3df1aa4
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-03 21:01:30 +0800

    resource time grid

--- app/assets/javascripts/controllers/booking/time_plans/calendar.js
+++ app/assets/javascripts/controllers/booking/time_plans/calendar.js
@@ -1,26 +1,14 @@
 var calendarEl = document.getElementById('calendar');
+
 var calendar = new FullCalendar.Calendar(calendarEl, {
-  plugins: [ 'dayGrid' ],
-  header: {
-    left: 'prev,next',
-    center: 'title',
-    right: ''
-  },
-  showNonCurrentDates: false,
-  fixedWeekCount: false,
-
-  // height
-  aspectRatio: 3,
-  height: 'auto',
-  firstDay: 1,
-
-  // events
-  eventSources: [
-    {
-      events: [],
-    color: '#8fdf82',
-  rendering: 'background'
-}
-]
+  schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+  plugins: [ 'dayGrid', 'timeGrid', 'resourceDayGrid', 'resourceTimeGrid' ],
+  defaultView: 'resourceTimeGridDay',
+  resources: [
+    { id: 'a', title: 'Auditorium A' },
+    { id: 'b', title: 'Auditorium B' },
+    { id: 'c', title: 'Auditorium C' }
+  ]
 });
+
 calendar.render();
--- app/assets/javascripts/controllers/booking/time_plans/index-ready.js
+++ app/assets/javascripts/controllers/booking/time_plans/index-ready.js
@@ -1,2 +1,6 @@
 //= require @fullcalendar/core
 //= require @fullcalendar/daygrid
+//= require @fullcalendar/timegrid
+//= require @fullcalendar/resource-common
+//= require @fullcalendar/resource-daygrid
+//= require @fullcalendar/resource-timegrid
