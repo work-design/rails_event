03c9aab0e6fab399598270fb0d4927a3265560f2
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-11 11:33:57 +0800

    WIP: time plan create js use redirect to

--- app/controllers/booking/time_plans_controller.rb
+++ app/controllers/booking/time_plans_controller.rb
@@ -29,7 +29,7 @@ class Booking::TimePlansController < Booking::BaseController
       if @time_plan.save
         format.html.phone
         format.html { redirect_to time_plans_url(params[:plan_type], params[:plan_id]), notice: 'Time plan was successfully created.' }
-        format.js { render :index }
+        format.js { redirect_to time_plans_url(params[:plan_type], params[:plan_id]) }
         format.json { render :show }
       else
         format.html.phone { render :new }
