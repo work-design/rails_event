960b265ab003616513c8c1936afa3b3f80cd8fa6
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-11 16:45:22 +0800

    sync planon

--- app/models/rails_booking/time_booking.rb
+++ app/models/rails_booking/time_booking.rb
@@ -11,16 +11,18 @@ module RailsBooking::TimeBooking
   
     delegate :start_at, :finish_at, to: :plan_item, allow_nil: true
     
+    validates :booking_on, presence: true
     validates :booker_id, uniqueness: { scope: [:booker_type, :plan_item_id] }
     
-    before_validation :sync_time_list_id
+    before_validation :sync_from_plan_item
     after_create :sync_to_booker
   end
   
-  def sync_time_list_id
+  def sync_from_plan_item
     if plan_item
       self.booked_type = self.plan_item.plan_type
       self.booked_id = self.plan_item.plan_id
+      self.booking_on = self.plan_item.plan_on
       self.time_item_id = self.plan_item.time_item_id
       self.time_list_id = self.time_item.time_list_id
     end
