b15da0cc61cb7b19e0fdbb0e921521e96bc4cfa1
qinmingyuan <mingyuan0715@foxmail.com>
2019-09-15 11:25:47 +0800

    fix as

--- app/models/rails_event/place.rb
+++ app/models/rails_event/place.rb
@@ -5,10 +5,13 @@ module RailsEvent::Place
     attribute :name, :string
     attribute :max_members, :integer
     
-    belongs_to :organ
-    attribute :time_plans_count, :integer, default: 0
+    attribute :plans_count, :integer, default: 0
 
-    has_many :time_plans
+    belongs_to :organ
+
+    has_many :plans
+    
+    validates :name, presence: true
     #TimePlan.belongs_to :place, class_name: self.name, foreign_key: :place_id, counter_cache: true, optional: true
   end
 
