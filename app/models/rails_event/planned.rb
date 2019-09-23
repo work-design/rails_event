6035df2e61768bc179c5b7203eca3ec6f0361b58
qinmingyuan <mingyuan0715@foxmail.com>
2019-09-23 18:05:38 +0800

    moved unused code

--- app/models/rails_event/planned.rb
+++ app/models/rails_event/planned.rb
@@ -5,12 +5,5 @@ module RailsEvent::Planned
     has_many :plans, as: :planned, dependent: :delete_all
     has_many :plan_items, as: :planned, dependent: :delete_all
   end
-
-  def default_plan(params)
-    plans.find_or_initilaze_by(
-      begin_on: params[:begin_on],
-      end_on: params[:end_on]
-    )
-  end
   
 end
