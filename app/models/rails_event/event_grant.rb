0073e76bbb6cc8b17edeafae5e6609543e839427
qinmingyuan <mingyuan0715@foxmail.com>
2020-05-27 18:00:20 +0800

    update grant

--- app/models/rails_event/event_grant.rb
+++ app/models/rails_event/event_grant.rb
@@ -1,12 +1,12 @@
 module RailsEvent::EventGrant
   extend ActiveSupport::Concern
-  
+
   included do
     attribute :grant_kind, :string
     attribute :grant_column, :string
-    attribute :filter, default: {}
-    
+    attribute :filter, :json, default: {}
+
     belongs_to :event
   end
-  
+
 end
