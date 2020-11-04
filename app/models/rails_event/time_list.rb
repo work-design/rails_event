e5fa3aa97e23559b209d14231208c2797cdc722b
qinmingyuan <mingyuan0715@foxmail.com>
2020-11-04 12:15:56 +0800

    can not use self

--- app/models/rails_event/time_list.rb
+++ app/models/rails_event/time_list.rb
@@ -7,14 +7,14 @@ module RailsEvent::TimeList
     attribute :item_minutes, :integer, default: 45
     attribute :interval_minutes, :integer, default: 15
     attribute :default, :boolean
-  
+
     belongs_to :organ, optional: true
     has_many :time_items, -> { order(start_at: :asc) }
     accepts_nested_attributes_for :time_items, allow_destroy: true
-    
-    after_update :set_default, if: -> { self.default? && saved_change_to_default? }
+
+    after_update :set_default, if: -> { default? && saved_change_to_default? }
   end
-  
+
   def set_default
     self.class.where.not(id: self.id).where(organ_id: self.organ_id).update_all(default: false)
   end
@@ -44,7 +44,7 @@ module RailsEvent::TimeList
       '18:00'
     end
   end
-  
+
   class_methods do
     def default
       self.find_by(default: true)
