820c7003edabf4ec07b5f8a571c5e195a0a053a1
qinmingyuan <mingyuan0715@foxmail.com>
2021-01-01 10:34:01 +0800

    moved js files

--- app/javascript/packs/rails_event/repeat_type.js
+++
@@ -1,9 +0,0 @@
-$('[data-title="repeat_type"]').dropdown({
-  onChange: function(value, text, $selectedItem){
-    var repeat_url = new URL(window.location.origin)
-    repeat_url.pathname = 'repeat_form'
-    repeat_url.search = $.param({booking_type: this.dataset['bookingType'], repeat_type: value})
-
-    Rails.ajax({url: repeat_url, type: 'GET', dataType: 'script'})
-  }
-})
