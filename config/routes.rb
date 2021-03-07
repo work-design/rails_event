10796888151b90df4668b2352d0902de706ca4ee
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-07 17:03:21 +0800

    lecen

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  scope module: :eventual, defaults: { business: 'eventual', namespace: 'application' } do
+  scope module: :eventual, defaults: { business: 'eventual' } do
     controller :time do
       get 'repeat_form', action: 'repeat_form'
     end
