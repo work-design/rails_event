6789f8a9dbb8629c3e91be27be5a7825fccacdaa
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-02 16:52:12 +0800

    gemil

--- lib/rails_event/engine.rb
+++ lib/rails_event/engine.rb
@@ -5,11 +5,12 @@ module RailsEvent
   class Engine < ::Rails::Engine
 
     config.autoload_paths += Dir[
-      "#{config.root}/app/models/rails_event/concerns",
       "#{config.root}/app/models/plan_participant"
     ]
 
-    config.factory_bot.definition_file_paths += Dir["#{config.root}/test/factories"] if defined?(FactoryBotRails)
+    config.eager_load_paths += Dir[
+      "#{config.root}/app/models/plan_participant"
+    ]
 
     config.generators do |g|
       g.rails = {
