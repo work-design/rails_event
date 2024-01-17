7ae54e9be9e28c27b171a2ea3d66648d997006fc
qinmingyuan <mingyuan0715@foxmail.com>
2024-01-17 23:12:35 +0800

    move debugsinfo

--- app/controllers/eventual/admin/events_controller.rb
+++ app/controllers/eventual/admin/events_controller.rb
@@ -20,14 +20,12 @@ module Eventual
       x = []
       @events.each do |event|
         event.next_days(start: params[:start], finish: params[:end]).each do |date|
-          puts date
           x << {
             start: date.to_fs(:date),
             classNames: ['bg_kapi'],
             display: 'background',
             extendedProps: { img: event.logo.url }
           }
-          puts x
         end
       end
 
